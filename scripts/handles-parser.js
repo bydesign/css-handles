0c51aafc6fe183f1c062f8687f5d88db0b326e3e
Joel Leineweber <jleineweber@answersingenesis.org>
2014-09-19 12:56:47 -0400

    css parser is now parsing values correctly

--- scripts/handles-parser.js
+++ scripts/handles-parser.js
@@ -7,8 +7,16 @@ var ps = {
 			ATRULE = 1,
 			PROPERTY = 2,
 			VALUE = 3,
-			COMMENT = 4;
+			COMMENT = 4,
+			FUNCTION = 5,
+			NUMBERVAL = 6,
+			TEXTVAL = 7,
+			COLORVAL = 8,
+			BANGVAL = 9,
+			UNITVAL = 10,
+			IMPORTANTVAL = 11;
 		
+		var COLORFNS = ['rgb','rgba','hsl','hsla'];
 		var lines = text.split('\n');
 		var parsed = {
 			rules: [],
@@ -18,8 +26,12 @@ var ps = {
 		var curRules = [];
 		var curProp = {};
 		var modes = [RULE];
-		var prevChar,
-			startPos;
+		var isNumberRegex = /[0-9-\.]/,
+			isTextRegex = /[a-zA-Z\%]/,
+			prevChar,
+			startPos,
+			vals = [],
+			subval = {};
 		for (i=0, lineCount=lines.length; i<lineCount; i++) {
 			var line = lines[i];
 			for (var j=0, charCount=line.length; j<charCount; j++) {
@@ -64,6 +76,25 @@ var ps = {
 					
 					} else if (mode == ATRULE) {
 						curToken += char;
+							
+					} else if (modes.indexOf(VALUE) != -1) {
+						if (mode == TEXTVAL) {
+							subval.text = curToken;
+							vals.push(subval);
+							modes.shift();
+							
+						} else if (mode == NUMBERVAL) {
+							subval.number = Number(curToken);
+							vals.push(subval);
+							modes.shift();
+						
+						} else if (mode == UNITVAL) {
+							subval.unit = curToken;
+							vals.push(subval);
+							modes.shift();
+						}
+						subval = {};
+						curToken = '';
 					}
 				
 				// start @rule
@@ -96,16 +127,58 @@ var ps = {
 				} else if (char == ':') {
 					if (mode == PROPERTY) {
 						modes[0] = VALUE;
-						curProp.name = curToken;
+						curProp.name = curToken.trim();
 						curToken = '';
+						//console.log(curProp.name);
 					} else {
 						curToken += char;
+					}
+					
+				// start function
+				} else if (char == '(') {
+					if (mode == TEXTVAL && [COLORFNS].indexOf(curToken) != -1) {
+						subval.type = 'color';
+						subval.fn = curToken;
+					}
+					//console.log('function name: ' + curToken);
+					modes.unshift(FUNCTION);
+					//curToken = '';
+						
+				// end function
+				} else if (char == ')') {
+					//console.log('function value: ' + curToken);
+					modes.shift();
+					//curToken = '';
+				
+				// parse commas
+				} else if (char == ',') {
+					//console.log('comma');
+					if (mode = FUNCTION) {
+						console.log(subval);
+						console.log('function parameter: ' + curToken);
+					} else if (mode = VALUE) {
+						console.log('multi-part value');
 					}
 				
 				// end property value
 				} else if (char == ';') {
-					modes[0] = PROPERTY;
-					curProp.value = curToken;
+					if (mode == UNITVAL) {
+						subval.unit = curToken;
+					} else if (mode == TEXTVAL) {
+						subval.text = curToken;
+					} else if (mode == NUMBERVAL) {
+						subval.number = Number(curToken);
+					} else if (mode == COLORVAL) {
+						subval.color = curToken;
+					}
+					
+					if (vals.length > 0) {
+						vals.push(subval);
+						curProp.value = vals;
+					
+					} else {
+						curProp.value = subval;
+					}
 					curProp.pos = {
 						start: {
 							line: startPos.line,
@@ -116,7 +189,12 @@ var ps = {
 							char: j
 						}
 					};
+					
 					curRules[0].properties.push(curProp);
+					modes.shift();
+					modes[0] = PROPERTY;
+					vals = [];
+					subval = {};
 					curProp = {};
 					curToken = '';
 				
@@ -126,6 +204,27 @@ var ps = {
 							line: i,
 							char: j-1
 						};
+						if (mode == VALUE) {
+							if (char.match(isNumberRegex)) {
+								modes.unshift(NUMBERVAL);
+								subval.type = 'number';
+							} else if (char.match(isTextRegex)) {
+								modes.unshift(TEXTVAL);
+								subval.type = 'text';
+							} else if (char == '#') {
+								modes.unshift(COLORVAL);
+								subval.type = 'color';
+							} else if (char == '!') {
+								modes.unshift(IMPORTANTVAL);
+								subval.type = 'important';
+							}
+						}
+					} else {
+						if (mode == NUMBERVAL && char.match(isTextRegex)) {
+							modes[0] = UNITVAL;
+							subval.number = Number(curToken);
+							curToken = '';
+						}
 					}
 					curToken += char;
 				}
